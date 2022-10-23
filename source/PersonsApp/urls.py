b53a18991fbc197b7d6b4ec94d1f7eab7bd1acab
sohaibssb <112778369+sohaibssb@users.noreply.github.com>
2022-10-23 20:33:22 +0300

    Update urls.py

--- source/PersonsApp/urls.py
+++ source/PersonsApp/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns=[
-    path('persons',views.person_LIST,name='person_LIST'),
-    path('persons',views.person_Post,name='person_Post'),
-    path('persons/<int:id>',views.person_detail,name='person_detail'),
-    path('persons/<int:id>',views.person_update,name='person_update'),
-    path('persons/<int:id>',views.person_remove,name='person_remove'),
+    path('persons',views.listPersons,name='listPersons'),
+    path('persons/<int:id>',views.aSpecificPersons,name='aSpecificPersons'),
 ]
 urlpatterns=format_suffix_patterns(urlpatterns)
